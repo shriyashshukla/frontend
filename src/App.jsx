987206487360b66ef9a3abf9876af57bfd35ab1e
shriyashshukla <harishukla797@gmail.com>
2023-08-04 18:53:18 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div>
+      <h1>ayush </h1>
+      <Home />
+      <Signup />
+      <Login/>
+    
+    </div>
+  );
+}
+
+export default App;
