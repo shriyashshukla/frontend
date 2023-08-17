32f7d31ae2cba5630dbf7ad6a08b42053b7e54a5
shriyashshukla <harishukla797@gmail.com>
2023-08-17 17:08:54 +0530

    database

---
+++ src/components/Browse.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+import laptopList from "./dummydata";
+const Browse=()=>{
+    const [laptopData,setLaptopData]=useState(laptopList);
+    const displayData =()=>{
+        return laptopData.map(()=>(
+
+        ))
+    }
+}
